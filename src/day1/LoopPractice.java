dc98d789998b0b3e2b8c752e786baa084f27cbfd
sengin <enginsema@gmail.com>
2020-02-29 13:59:54 -0700

    Updated loop class

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -1,11 +1,14 @@
 package day1;
 
+import javax.sound.midi.Soundbank;
+
 public class LoopPractice {
     public static void main(String[] args) {
         //some random code
         for (int i = 0; i < 10 ; i+=2) {
             System.out.println("Counter :: " + i);
             System.out.println("i = " + i);
+            System.out.println(" Git is very fun !!! ");
         }
     }
 }
